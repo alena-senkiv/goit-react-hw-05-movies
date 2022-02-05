b0ee68d1cd5cb32cf73448aa848590b0d4b9770b
alena-senkiv <alena.senkiv@gmail.com>
2022-02-05 11:23:42 +0200

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from 'components/App/App';
 import { GlobalStyle } from 'styles/GlobalStyle.styled';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
